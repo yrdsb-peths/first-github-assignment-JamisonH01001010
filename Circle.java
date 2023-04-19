7b33bab04c7ac1fe7ace8ab15d4f36c9842a65c8
JamisonH01001010 <130993463+JamisonH01001010@users.noreply.github.com>
2023-04-19 08:38:32 -0400

    Created Circle class

---
+++ Circle.java
@@ -0,0 +1,30 @@
+public class Circle(){
+  private int radius;
+  private String color;
+  public static final double PI = 3.14;
+  
+  public Circle(int radius, String color){
+    radius = this.radius;
+    color = this.color;
+  }
+  
+  public int getRadius(){
+    return radius;
+  }
+  
+  public String getColor(){
+    return color;
+  }
+  
+  public double getArea(){
+    return radius * PI * PI;
+  }
+  
+  public double getCircumference(){
+    return radius * 2 * PI;
+  }
+  
+  public String toString(){
+    return "Circle with radius " + radius;
+  }
+}
